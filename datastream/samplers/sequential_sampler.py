c513aabccead2bcc5f65ae72a686d313ba136dd9
FelixAbrahamsson <felixab@kth.se>
2020-10-30 11:53:58 +0100

    feature: add sequentialsampler

---
+++ datastream/samplers/sequential_sampler.py
@@ -0,0 +1,27 @@
+from __future__ import annotations
+from pydantic import BaseModel
+import torch
+
+
+class SequentialSampler(BaseModel, torch.utils.data.Sampler):
+    sampler: torch.utils.data.SequentialSampler
+
+    class Config:
+        arbitrary_types_allowed = True
+        allow_mutation = False
+
+    def __init__(self, length):
+        BaseModel.__init__(
+            self,
+            sampler=torch.utils.data.SequentialSampler(torch.ones(length))
+        )
+
+    def __len__(self):
+        return len(self.sampler)
+
+    def __iter__(self):
+        return iter(self.sampler)
+
+    def sample_proportion(self, proportion):
+        sampler = SequentialSampler(int(len(self) * proportion))
+        return sampler
