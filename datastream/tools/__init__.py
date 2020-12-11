82a8ae74ef3f56c08aecd67220ae47cded0c5476
FelixAbrahamsson <felixab@kth.se>
2020-12-11 12:41:29 +0100

    refactor: move split functions from dataset to tools

--- datastream/tools/__init__.py
+++ datastream/tools/__init__.py
@@ -3,3 +3,5 @@ from datastream.tools.starcompose import starcompose
 from datastream.tools.repeat_map_chain import repeat_map_chain
 from datastream.tools.numpy_seed import numpy_seed
 from datastream.tools.split_dataframes import split_dataframes
+from datastream.tools.unstratified_split import unstratified_split
+from datastream.tools.stratified_split import stratified_split
