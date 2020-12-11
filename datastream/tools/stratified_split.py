9861d963c527acd47d1fd762be4fd18cfe5c89d6
FelixAbrahamsson <felixab@kth.se>
2020-12-11 12:41:29 +0100

    refactor: move split functions from dataset to tools

---
+++ datastream/tools/stratified_split.py
@@ -0,0 +1,50 @@
+import warnings
+from typing import Dict, Optional
+from pathlib import Path
+from datastream import tools
+
+
+def stratified_split(
+    dataset,
+    key_column: str,
+    proportions: Dict[str, float],
+    stratify_column: Optional[str] = None,
+    save_directory: Optional[Path] = None,
+    seed: Optional[int] = None,
+    frozen: Optional[bool] = False,
+):
+    if (
+        stratify_column is not None
+        and any(dataset.dataframe[key_column].duplicated())
+    ):
+        # mathematically impossible in the general case
+        warnings.warn(
+            'Trying to do stratified split with non-unique key column'
+            ' - cannot guarantee correct splitting of key values.'
+        )
+    strata = {
+        stratum_value: dataset.subset(
+            lambda df: df[stratify_column] == stratum_value
+        )
+        for stratum_value in dataset.dataframe[stratify_column].unique()
+    }
+    split_strata = [
+        tools.unstratified_split(
+            stratum,
+            key_column=key_column,
+            proportions=proportions,
+            filepath=(
+                save_directory / f'{hash(stratum_value)}.json'
+                if save_directory is not None else None
+            ),
+            seed=seed,
+            frozen=frozen,
+        )
+        for stratum_value, stratum in strata.items()
+    ]
+    return {
+        split_name: type(dataset).concat(
+            [split_stratum[split_name] for split_stratum in split_strata]
+        )
+        for split_name in proportions.keys()
+    }
