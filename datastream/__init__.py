b6b326aa486c4f4f41e79832357641da56e79768
Richard Löwenström <samedii@gmail.com>
2023-11-28 16:03:56 +0100

    improve: support pydantic 2

--- datastream/__init__.py
+++ datastream/__init__.py
@@ -1,9 +1,2 @@
 from datastream.dataset import Dataset
 from datastream.datastream import Datastream
-
-from pkg_resources import get_distribution, DistributionNotFound
-
-try:
-    __version__ = get_distribution("pytorch-datastream").version
-except DistributionNotFound:
-    pass
